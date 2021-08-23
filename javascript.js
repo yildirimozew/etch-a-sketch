62554c667bc2223cbfbfc5cd357f7483351a02c1
yildirimozew <yildirimozew@hotmail.com>
2021-08-24 01:04:18 +0300

    Added user interface and a canvas

--- javascript.js
+++ javascript.js
@@ -1,8 +1,31 @@
 let containerDiv = document.getElementById("container");
+let colorChanger = document.getElementById("rgb-picker");
+let rainbow = 0; 
 
-for (let i = 0; i < 256; i++) {
+function drawCanvas() {
+    let squareNum = document.getElementById("squareNum").value;
+    containerDiv.innerHTML = "";
+for (let i = 0; i < squareNum ** 2; i++) {
     let squareDiv = document.createElement("div");
     squareDiv.classList.add("sketch");
-    squareDiv.style.height = "6.25vh";
-    squareDiv.style.width = "6.25vh";
-    containerDiv.appendChild(squareDiv);}
+    let percentage = 100 / squareNum;
+    squareDiv.style.height = percentage + "vh";
+    squareDiv.style.width = percentage + "vh";
+    squareDiv.addEventListener("mouseover", () => {if (rainbow === 1) {let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        squareDiv.style.backgroundColor = randomColor;}
+    else {squareDiv.style.backgroundColor = color;}})
+    containerDiv.appendChild(squareDiv);}}
+
+function colorChange() {
+    rainbow = 0;
+    color = colorChanger.value;
+}
+
+function init() {
+    colorChange();
+    drawCanvas();
+}
+
+function rainbowize() {if (rainbow === 0 ) {rainbow = 1}};
+
+init();
